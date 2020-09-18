832fee7c7511a9ccbb46ceffc0194d8ad38840bb
Ghost <malinga.simuchimba@gmail.com>
2020-09-18 10:59:06 +0200

    update

--- classes/User.php
+++ classes/User.php
@@ -71,6 +71,9 @@ class User
         $this->email = $data['email'];
         $this->password = $data['password'];
 
+        print_r($data);
+        die();
+
         //get user by email
         $user = $this->getUserByEmail()->fetch_assoc();
 
--- server/Database.php
+++ server/Database.php
@@ -15,17 +15,27 @@ class Database
     public function connect()
     {
         //set the variables
-        $this->host = "localhost";
+        /*$this->host = "localhost";
         $this->username = "root";
         $this->password = "";
-        $this->db = "todos";
+        $this->db = "todos";*/
 
-        $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->db);
+        //remote server
+        $this->host = "sql306.epizy.com";
+        $this->username = "epiz_26755204";
+        $this->password = "14102897Simz@";
+        $this->db = "epiz_26755204_todosApi";
 
-        if ($this->conn->connect_errno) {
-            echo $this->conn->connect_error;
-        } else {
-            return $this->conn;
+        try {
+            $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->db);
+
+            if ($this->conn->connect_errno) {
+                echo $this->conn->connect_error;
+            } else {
+                return $this->conn;
+            }
+        } catch (Exception $e) {
+            echo  $e->getMessage();
         }
     }
 }
